5c942cdefcd55af6245b90e6385509e49d09ab47
conormorgan <morgan.fourteen@hotmail.co.uk>
2021-12-03 13:34:04 +0000

    part 1 solution

--- day1/part1.py
+++ day1/part1.py
@@ -1,4 +1,15 @@
-with open('sample.txt') as file:
-    values = file.read().splitlines()
+with open('test.txt') as file:
+    depths = file.read().splitlines()
 
-print(values)
+increases = 0
+
+for i in range(1,len(depths)):
+        if depths[i] > depths[i-1] :
+            increases += 1
+
+count = sum(y>x for x,y in zip(depths, depths[1:]))
+print(count)
+print(increases)
+
+list_comp = len([depths[i] for i in range(1,len(depths)) if depths[i] > depths[i-1]])
+print(list_comp)
